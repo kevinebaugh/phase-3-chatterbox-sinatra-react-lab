a51ddf3bb030edc334c5a6b4845b40ab726fcfd3
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-03-09 20:44:48 -0500

    commit tree 5ed0aa46e8d71087fe0f2bef065e2677227ce97e

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,40 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add routes
-  
+
+  # GET /messages: returns an array of all messages as JSON, ordered by created_at
+  # in ascending order.
+  get "/messages" do
+    Message.all.order(created_at: :ASC).to_json
+  end
+
+  # POST /messages: creates a new message with a body and username from params,
+  # and returns the newly created post as JSON.
+
+  post "/messages" do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  # PATCH /messages/:id: updates the body of the message using params, and
+  # returns the updated message as JSON.
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+
+    message.update(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  # DELETE /messages/:id: deletes the message from the database.
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.delete
+    message.to_json
+  end
+
 end
